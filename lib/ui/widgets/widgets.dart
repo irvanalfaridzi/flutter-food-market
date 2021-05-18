ffe254d08c49142886253194d3ca08356d7e58fd
irvanalfaridzi@gmail.com <irvanalfaridzi@gmail.com>
2021-05-18 15:48:56 +0700

    food tab page

--- lib/ui/widgets/widgets.dart
+++ lib/ui/widgets/widgets.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:food_market_alfa/models/models.dart';
 import 'package:food_market_alfa/shared/shared.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 import 'package:supercharged/supercharged.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
@@ -9,3 +10,4 @@ part 'custom_bottom_navbar.dart';
 part 'food_card.dart';
 part 'rating_stars.dart';
 part 'custom_tabbar.dart';
+part 'food_list_item.dart';
