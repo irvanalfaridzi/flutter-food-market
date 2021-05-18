bf1fc5077ff3ff65a2e46fadde4f60e4887edc99
irvanalfaridzi@gmail.com <irvanalfaridzi@gmail.com>
2021-05-18 15:16:56 +0700

    custom tab bar

--- lib/models/food.dart
+++ lib/models/food.dart
@@ -20,14 +20,60 @@ class Food {
   });
 }
 
-Food mockFood = Food(
-  id: 1,
-  picturePath:
-      "https://i.pinimg.com/564x/07/75/a5/0775a5b2e05c5f5296951382e0107107.jpg",
-  name: "Sate Sayur Sultan",
-  description:
-      "Sate adalah makanan khas Bandung yang cukup sering dipesan oleh anak muda dengan pola makan yang cukup tinggi dengan mengutamakan diet yang sehat dan teratur.",
-  ingredients: "Bawang Merah, Bawang Putih, Paprika, Bawang Bombay, TImun",
-  price: 150000,
-  rate: 4.2,
-);
+List<Food> mockFoods = [
+  Food(
+    id: 1,
+    picturePath:
+        "https://i.pinimg.com/564x/07/75/a5/0775a5b2e05c5f5296951382e0107107.jpg",
+    name: "Sate Sayur Sultan",
+    description:
+        "Sate adalah makanan khas Bandung yang cukup sering dipesan oleh anak muda dengan pola makan yang cukup tinggi dengan mengutamakan diet yang sehat dan teratur.",
+    ingredients: "Bawang Merah, Bawang Putih, Paprika, Bawang Bombay, TImun",
+    price: 150000,
+    rate: 4.2,
+  ),
+  Food(
+    id: 2,
+    picturePath:
+        "https://i.pinimg.com/564x/e3/d0/1b/e3d01be8b77d951b0ba79ea8e3cdc84e.jpg",
+    name: "Sate Sosis Yahut",
+    description:
+        "Maknyus sekaliu sate ini dengan perpaduan rasa yang sangat khas dan nikmat sekali, sangat enak dimakan saat panas maupu  dingin, sungguh mantap sekali",
+    ingredients: "Bawang Putih, Paprika, Bawang Bombay, TImun",
+    price: 170000,
+    rate: 3.6,
+  ),
+  Food(
+    id: 3,
+    picturePath:
+        "https://i.pinimg.com/564x/6e/b3/ec/6eb3ecc4327dd7f0aa7e6a9166c5da46.jpg",
+    name: "Kentang Goreng Yahut",
+    description:
+        "Sate adalah makanan khas Bandung yang cukup sering dipesan oleh anak muda dengan pola makan yang cukup tinggi dengan mengutamakan diet yang sehat dan teratur.",
+    ingredients: "Bawang Merah, Bawang Putih, Paprika, Bawang Bombay, TImun",
+    price: 120000,
+    rate: 4.9,
+  ),
+  Food(
+    id: 4,
+    picturePath:
+        "https://i.pinimg.com/564x/2c/f4/27/2cf427ff858ce80432446c1ebb3bfda4.jpg",
+    name: "Mie Goreng Empal",
+    description:
+        "Mie gorenge mpal yange ankse eskaselam sdajhfbas djhasd fasd asdjhfbsjadhfb dsjhga sdjfha sdf asd fjahsdf a sdjfha sdfjhas dfjahsdf jasdhf jashdfbjkashdgbasdg asdkgj basdf",
+    ingredients: "Bawang Merah, Bawang Putih, Paprika, Bawang Bombay, TImun",
+    price: 200000,
+    rate: 4.0,
+  ),
+  Food(
+    id: 5,
+    picturePath:
+        "https://i.pinimg.com/564x/34/68/3e/34683e03c1b0beb32dd145d80588e72f.jpg",
+    name: "Tacos",
+    description:
+        "kjsdnkasjdg afsdkj asdfg asd akjsihd fasd fkjasd fjasdf hjsfdbglskdjfgnsdf skdjfgnowiuerhtwern gkisudrfbgawerug sd,mv iwseur vskidrv erugbesmrn vsjkdhfvsd iskdur gsiekrg sdkirvjgsdkfjv sdfkin",
+    ingredients: "Bawang Merah, Bawang Putih, Paprika, Bawang Bombay, TImun",
+    price: 127000,
+    rate: 4.3,
+  ),
+];
--- lib/ui/pages/food_page.dart
+++ lib/ui/pages/food_page.dart
@@ -6,6 +6,7 @@ class FoodPage extends StatefulWidget {
 }
 
 class _FoodPageState extends State<FoodPage> {
+  int selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
     return ListView(
@@ -54,8 +55,69 @@ class _FoodPageState extends State<FoodPage> {
               ),
             ),
 //// LIST OF FOOD
-            FoodCard(mockFood),
+            Container(
+              height: 258,
+              width: double.infinity,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Row(
+                    children: mockFoods
+                        .map((e) => Padding(
+                              padding: EdgeInsets.only(
+                                  right: defaultMargin,
+                                  left: (e == mockFoods.first)
+                                      ? defaultMargin
+                                      : 0),
+                              child: FoodCard(e),
+                            ))
+                        .toList(),
+                  )
+                ],
+              ),
+            ),
 //// LIST OF FOOD (TABS)
+            Container(
+              width: double.infinity,
+              color: Colors.white,
+              child: Column(
+                children: [
+                  CustomTabBar(
+                    titles: [
+                      "New Taste",
+                      "Popular",
+                      "Rcommended",
+                    ],
+                    selectedIndex: selectedIndex,
+                    onTap: (index) {
+                      setState(() {
+                        selectedIndex = index;
+                      });
+                    },
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Builder(builder: (_) {
+                    String body = (selectedIndex == 0)
+                        ? "New Taste Body"
+                        : (selectedIndex == 1)
+                            ? "Popular Body"
+                            : "Recommended Body";
+
+                    return Center(
+                      child: Text(
+                        body,
+                        style: blackFontStyle2,
+                      ),
+                    );
+                  }),
+                  SizedBox(
+                    height: 80,
+                  )
+                ],
+              ),
+            )
           ],
         )
       ],
